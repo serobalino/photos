bab4f8c61d047937499411d57f5b7e4a66a5302e
Sebastian Robalino <odm_sd@hotmail.com>
2019-11-19 23:08:05 +0000

    Creacion del Proyecto

---
+++ storage/configuration/database.php
@@ -0,0 +1,9 @@
+<?php
+	return array(
+		'hostname' => 'localhost',
+		'database' => 'u538496321_wf',
+		'username' => 'u538496321_wf',
+		'password' => ':LzcGeo1c|=sTGB42C',
+		'prefix' => 'wf_',
+		'socket' => ''
+	);
---
+++ storage/configuration/key.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Do not edit or remove this file unless advised by Koken support
+// support@koken.me
+
+return 'f05dcf328711e5efcbc314798728e776';
